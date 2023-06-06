3be895b56766d0225c041003c2b2d50478ce9cf8
Vade-20 <vedantsaxena2012@gmail.com>
2023-06-06 09:59:50 +0530

    Final

--- main_game.py
+++ main_game.py
@@ -6,7 +6,7 @@ from hangman_visual import visuals
 from tkinter import messagebox
 
 dictionary = PyDictionary()
-with open('words.json', 'r') as f:
+with open('words.json', 'r',encoding='utf-8') as f:
     data = json.load(f)
 
 root = Tk()
@@ -35,17 +35,17 @@ def right_word(n=None):
     global rand, l3
     rand = choice(list(data)).lower()
     meaning = dictionary.meaning(rand, disable_errors=True)
-    if meaning is None:
-        return right_word()
-    else:
-        ans = '__  ' * len(rand)
-        l3 = Label(root, fg='red', bg='black', font=('Times', '20'), justify='center', text=ans, border=3, relief='raised')
-        l3.grid(row=5, column=0, columnspan=6, sticky=W+E)
-        for i, j in enumerate(meaning, start=6):
-            t = Text(root, fg='red', bg='black', font=('Times', '15'), height=2, wrap='word')
-            t.grid(row=i, column=0, columnspan=6, sticky=W+E)
-            t.insert(END, f'{j} : {",".join(meaning[j])}')
-            break
+    while meaning is None:
+        rand = choice(list(data)).lower()
+        meaning = dictionary.meaning(rand, disable_errors=True)
+    ans = '__  ' * len(rand)
+    l3 = Label(root, fg='red', bg='black', font=('Times', '20'), justify='center', text=ans, border=3, relief='raised')
+    l3.grid(row=5, column=0, columnspan=6, sticky=W+E)
+    for i, j in enumerate(meaning, start=6):
+        t = Text(root, fg='red', bg='black', font=('Times', '15'), height=2, wrap='word')
+        t.grid(row=i, column=0, columnspan=6, sticky=W+E)
+        t.insert(END, f'{j} : {",".join(meaning[j])}')
+        break
 
 def new_game(n=None):
     global chances, b1_f1, l, value, l1_f1, l2_f2, l3_f1, e1
@@ -61,19 +61,22 @@ def new_game(n=None):
                highlightbackground='red', justify='center')
     e1.grid(row=4, column=1, sticky=W+E)
 
-    l1_f1 = Label(f1, text=f'You have {chances} chances left ', fg='red',
-                  bg='black', font=('Times', '15'),)
-    l1_f1.grid(row=0, column=0, sticky=W)
-    l3_f1 = Text(f1, fg='red', wrap='word', bg='black', font=('Times', '15'), width=15, height=10)
-    l3_f1.insert(END, 'Character already used:')
-    l3_f1.grid(row=1, column=2, sticky=W)
-    l2_f2 = Text(f1, fg='red', wrap='word', height=7, bg='black', font=('Times', '20'))
-    l2_f2.insert(END, f'{visuals[chances]}')
-    l2_f2.grid(row=1, column=0, sticky=W+E, columnspan=2)
     b1_f1 = Button(f1, text='New Game', fg='red', bg='black', width=20, relief='raised', font=('Times', '16'),
                    command=new_game)
     b1_f1.grid(row=0, column=1, sticky=E, columnspan=2)
-
+    
+    l1_f1 = Label(f1, text=f'You have {chances} chances left ', fg='red',
+                  bg='black', font=('Times', '15'),)
+    l1_f1.grid(row=0, column=0, sticky=W)
+    
+    l2_f2 = Text(f1, fg='red', wrap='word', height=7, bg='black', font=('Times', '20'))
+    l2_f2.insert(END, f'{visuals[chances]}')
+    l2_f2.grid(row=1, column=0, sticky=W+E, columnspan=2)
+    
+    l3_f1 = Text(f1, fg='red', wrap='word', bg='black', font=('Times', '15'), width=15, height=10)
+    l3_f1.insert(END, 'Character already used:')
+    l3_f1.grid(row=1, column=2, sticky=W)
+    
     value = StringVar()
     value.set('Select an Alphabet')
     s1 = OptionMenu(root, value, *list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), command=check)
@@ -114,8 +117,10 @@ l1.grid(row=0, column=0, columnspan=6)
 
 f1 = Frame(root, bg='black')
 f1.grid(row=1, column=0, columnspan=6, rowspan=3)
+
 b1_f1 = Button(f1, text='Start Game', fg='red', bg='black', relief='raised', font=('Times', '30'), command=new_game)
 b1_f1.grid(row=1, column=0, sticky=W+E, columnspan=2)
+
 l = Label(f1, text='Note: This may take a few seconds to load', fg='red', bg='black', font=('Times', '15'))
 l.grid(row=2, column=0, columnspan=6, sticky=W+E)
 
